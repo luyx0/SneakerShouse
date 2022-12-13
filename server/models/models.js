494a97abb029762ab5089a3d6d28c835ea1304f6
Arsen Romanenko <102360448+luyx0@users.noreply.github.com>
2022-12-13 06:01:34 +0300

    Create models.js

---
+++ server/models/models.js
@@ -0,0 +1,40 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true,},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketDevice = sequelize.define('basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define('device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.INTEGER, unique: true, allowNull: true},
+    price: {type: DataTypes.INTEGER, allowNull: true},
+    img: {type: DataTypes.INTEGER, allowNull: true},
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
+
+module.exports = {
+    User,
+    Basket,
+    BasketDevice,
+    Device
+}
